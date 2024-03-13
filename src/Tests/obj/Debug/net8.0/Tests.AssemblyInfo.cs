0a0b8bb3acb252f9329f2fe391a8eba33c0ad352
marco <maffalcao@gmail.com>
2024-03-13 15:16:59 -0300

    'gitignore'

--- src/Tests/obj/Debug/net8.0/Tests.AssemblyInfo.cs
+++ src/Tests/obj/Debug/net8.0/Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5476e6a232eec5ef218846fc78464fda86ddd00f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+498b7195a74c90453cbcee9651e61a479683b73a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
